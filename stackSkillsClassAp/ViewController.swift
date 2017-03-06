d4fced99205c33293eeea76b98e966cd780460c3
Zach Fletcher <zacharyafletcher@gmail.com>
2017-03-05 23:57:20 -0500

    Can add or subtract now.

--- stackSkillsClassAp/ViewController.swift
+++ stackSkillsClassAp/ViewController.swift
@@ -9,30 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
-
     
     @IBAction func myButton(_ sender: Any) {
-  
-        myLabel.text = "Sum equal to \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        let addition = false
+        
+        if addition {
+            myLabel.text = "Operation result equal to \(Double(text1.text!)! + Double(text2.text!)!)"
+        } //end if
+        else {
+            myLabel.text = "Operation result equal to \(Double(text1.text!)! - Double(text2.text!)!)"
+        } //end else
     } //
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "No data yet..."
+        
     } //end viewDidLoad
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     } //end didReceiveMemoryWarning
-
-
+    
+    
 } //end UIViewController
 
